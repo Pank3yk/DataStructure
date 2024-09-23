33d13d4d5d3e3943f5a35842e2ad4f9f19db24e1
qlgarcia <qlgarcia02@tip.edu.ph>
2024-09-23 15:06:04 +0800

    Update Action.java

--- Action.java
+++ Action.java
@@ -1,17 +1,1 @@
-public class Action {
-    private String type;
-    private Book book;
 
-    public Action(String type, Book book) {
-        this.type = type;
-        this.book = book;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public Book getBook() {
-        return book;
-    }
-}
